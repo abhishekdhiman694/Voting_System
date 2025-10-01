4156676c601a4a313a2c044d88c6bff3d9074cba
abhishekdhiman694 <dhimanabhishek600@gmail.com>
2025-10-02 01:18:43 +0530

    update

---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,20 @@
+
+// src/components/Navbar.js
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <h1 className="navbar-title">ELECTIFY</h1>
+      <ul className="navbar-links">
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/aboutus">About Us</Link></li>
+        <li><Link to="/signup">Sign Up</Link></li>
+        
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
